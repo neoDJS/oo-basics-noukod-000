caae760b115e8f34b07b6180ca28e8f5419ad269
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-10-03 15:00:31 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,8 @@
 # Make your shoe class here!
 class shoe
   attr_accessor :brand, :color, :size, :material, :condition
+  
   def initialize(brand= "")
     @brand = brand
   end
-
-  #def repaire
 end
